c25a3d481e3b2caab0f6bd8b791180d79bee3010
pooja.katke <pooja.katke@techjivaaindia.in>
2023-07-12 13:58:16 +0530

    Add tabular in self Reg

--- src/app/features/self-registration/self-registration.component.ts
+++ src/app/features/self-registration/self-registration.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import selfRegForm from './selfRegForm.json';
-import selfRegbreadcrumb from './selfReg_breadcrumb.json'
+import selfRegbreadcrumb from './selfReg_breadcrumb.json';
+import tabular from './selfRegTabular.json';
+import addressForm from './AddressForm.json';
+import medicalForm from './MedicalForm.json';
 import { CommonService } from 'src/app/core/shared/service/common.service';
 import { SelfRegistrationService } from './self-registration.service';
 
@@ -23,10 +26,98 @@ export class SelfRegistrationComponent implements OnInit {
   flag: any;
   errorFlag: boolean = false;
   editData: any
+  tabularFormData = tabular
+  addressEditData: any;
+  medicalEditData: any;
+  formData1: any =addressForm;
+  formData2: any ;
+  formData3: any;
+  paramObj: any = {
+    "patientIdentificationTypeId": "",
+    "patientIdentificationTypeName": "",
+    "patientTitleId": "",
+    "patientTitleName": "",
+    "patientFirstname": "",
+    "patientMiddlename": "",
+    "patientLastname": "",
+    "patientDob": "",
+    "patientAge": "",
+    "patientMaritalStatusId": "",
+    "patientMaritalStatusName": "",
+    "profileImage": "",
+    "patientMobileNumber": "",
+    "patientAddressLine1": "",
+    "patientAddressLine2": "",
+    "patientCityId": "",
+    "patientCityName": "",
+    "patientStateId": "",
+    "patientStateName": "",
+    "patientCountryId": "",
+    "patientCountryName": "",
+    "patientBloodGroupId": "",
+    "patientBloodGroupName": "",
+    "patientEthinicityId": "",
+    "patientEthinicityName": "",
+    "patientReligion": "",
+    "patientBlock": "",
+    "patientPrnNumber": "",
+    "patientPrivilageId": "",
+    "patientPrivilageName": "",
+    "patientInsuranceNumber": "",
+    "patientInsurancePolicyNumber": "",
+    "patientInsuranceCompanyNumber": "",
+    "patientInsuranceCompanyName": "",
+    "patientIsTobacoConsume": false,
+    "patientIsTobacoConsumeYear": "",
+    "patientIsAlcoholConsume": false,
+    "patientIsAlcoholConsumeYear": "",
+    "patientOccupation": "",
+    "patientReferredBy": "",
+    "patientLanguages": "",
+    "patientPhoneNumber": "",
+    "patientUploadImage": "",
+    "patientRegistrationSource": "",
+    "patientSocialStatusId": "",
+    "patientSocialStatusName": "",
+    "patientHealthId": "",
+    "patientHealthNumber": "",
+    "patientEmail": ""
+
+  };
   constructor(private messageService: MessageService, private http: SelfRegistrationService) { }
 
   ngOnInit(): void {
 
+  }
+
+  tabularValue(e: any) {
+    if (e.id == 0) {
+      this.formData1 = addressForm;
+      this.formData2 = "";
+    } else if (e.id == 1) {
+      this.formData1 = "";
+      this.formData2 = medicalForm;
+    } 
+  }
+
+  saveAddressData(e: any) {
+    this.paramObj = {
+      "patientStateId": [e.selectState[0], e.selectState[1]],
+
+    }
+    this.messageService.add({ severity: 'success', summary: 'success', detail: 'Save And Continue..' });
+    console.log("saveRegistartionForm => ", this.paramObj);
+  }
+  saveMedicalData(e: any) {
+    this.paramObj = {
+      "patientIsTobacoConsume": e.alcoholRad,
+      "patientIsTobacoConsumeYear": e.alcoholQuestion1,
+      "patientIsAlcoholConsume": e.tobacooRad,
+      "patientIsAlcoholConsumeYear": e.tobacooQuestion1,
+
+    }
+    this.messageService.add({ severity: 'success', summary: 'success', detail: 'Save And Continue.' });
+    console.log("saveRegistartionForm => ", this.paramObj);
   }
 
   buttonEvent(e: any) {
@@ -34,19 +125,19 @@ export class SelfRegistrationComponent implements OnInit {
   }
   sidebarData(e: any) {
     if (e != 'reset') {
-      const param = {     
+      const param = {
         "patientTitleId": e.patientTitleId,
         "patientTitleName": e.selectTitle,
         "patientFirstname": e.firstName,
         "patientMiddlename": e.middleName,
         "patientLastname": e.lastName,
         "patientAge": e.age,
-        "patientMobileNumber":e.mobileNo,
+        "patientMobileNumber": e.mobileNo,
         "patientAddressLine1": e.address,
         "patientCityId": e.state[2],
         "patientCityName": e.patientCityName,
         "patientStateId": e.state[0],
-        "patientRegistrationSource":"Self"
+        "patientRegistrationSource": "Self"
       };
       this.http.saveSelfRegData(param).subscribe((resData) => {
         this.data = undefined;
