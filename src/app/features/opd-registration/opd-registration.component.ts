6a594de5075c1f9fc2bdc1cd2c93988aa724f493
Devika Sutar <devika.sutar@techjivaaindia.in>
2023-07-26 17:32:46 +0530

    Create a new service table and bind service API for OPD

--- src/app/features/opd-registration/opd-registration.component.ts
+++ src/app/features/opd-registration/opd-registration.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import opdRgistrationForm from './opdRegistrationForm.json';
+import configService from './service_table_config.json';
+import { OpdRegistrationService } from './opd-registration.service';
 
 
 @Component({
@@ -9,12 +11,33 @@ import opdRgistrationForm from './opdRegistrationForm.json';
 })
 export class OpdRegistrationComponent implements OnInit {
   formJSON:any=opdRgistrationForm;
-  constructor() { }
+  serviceConfig:any;
+  configServ:any;
+  opdData: any[];
+  constructor(private http: OpdRegistrationService) { }
 
   ngOnInit(): void {
+    this.serviceConfig = {
+      "isFilter": false,
+      "isTable": true,
+      "isSideBar": false,
+      "isConfirmation": false
+    };
+    this.getConfigForTable();
+    this.getOPDService();
+  }
+  getConfigForTable() {
+    this.configServ = configService;
   }
   changeEvent(e:any){
 
+  }
+  getOPDService() {
+    this.http.getOPDService().subscribe((res) => {
+      this.opdData = [];
+      this.opdData = res[1].result;
+      console.log("get all data getOPDService ====>>>>", res)
+    });
   }
 
 }
--- src/app/features/opd-registration/opd-registration.service.ts
+++ src/app/features/opd-registration/opd-registration.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpdRegistrationService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  PostCall(url: any, param: any): Observable<any> {
+    const headers = new HttpHeaders().set('Content-type', 'application/json');
+    return this.http.post<any>(url, param, { headers })
+  }
+
+  getOPDService(){
+    const url = "http://localhost:8087/mstSubServices/listByDepartmentId/1";
+    return this.http.get<any>(url);
+  }
 }
